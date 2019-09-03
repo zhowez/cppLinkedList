56374c602bae92cc775f67a8866254732b49cdea
Zach <zhowes14@icloud.com>
2019-09-03 18:59:21 -0400

    all functions should be implemented in list.h

--- cppLinkedList/List.h
+++ cppLinkedList/List.h
@@ -16,121 +16,265 @@ using namespace std;
 //Do not modify anything in the class interface
 template <class T>
 class List{
-
- private:
-  Node<T> * start; //pointer to the first node in this list
-  int mySize;  //size (or length) of this list
-
- public:
-  List();
-  ~List();
-  int size();
-  bool empty();
-  void insertStart(T);
-  void insertEnd(T);
-  void insertAt(T, int);
-  void removeStart();
-  void removeEnd();
-  void removeAt(int);
-  T getFirst();
-  T getLast();
-  T getAt(int);
-  int find(T);
-
-  //Print the name and this list's size and values to stdout
-  //This function is already implemented (no need to change it)
-  void print(string name){
-    cout << name << ": ";
-    cout << "size = " << size();
-    cout << ", values = ";
-    Node<T> * iterator = start;
-    while(iterator != nullptr){
-      cout << iterator->value << ' ';
-      iterator = iterator->next;
+    
+private:
+    Node<T> * start; //pointer to the first node in this list
+    int mySize;  //size (or length) of this list
+    
+public:
+    List();
+    ~List();
+    int size();
+    bool empty();
+    void insertStart(T);
+    void insertEnd(T);
+    void insertAt(T, int);
+    void removeStart();
+    void removeEnd();
+    void removeAt(int);
+    T getFirst();
+    T getLast();
+    T getAt(int);
+    int find(T);
+    
+    //Print the name and this list's size and values to stdout
+    //This function is already implemented (no need to change it)
+    void print(string name){
+        cout << name << ": ";
+        cout << "size = " << size();
+        cout << ", values = ";
+        Node<T> * iterator = start;
+        while(iterator != nullptr){
+            cout << iterator->value << ' ';
+            iterator = iterator->next;
+        }
+        cout << endl;
     }
-    cout << endl;
-  }
-
+    
 }; //end of class interface (you may modify the code below)
 
 //Implement all of the functions below
 //Construct an empty list by initializig this list's instance variables
 template <class T>
 List<T>::List(){
-    this.start
+    mySize = 0;
+    this->start = NULL;
 }
 
 //Destroy all nodes in this list to prevent memory leaks
 template <class T>
 List<T>::~List(){
+    if (this->mySize > 0){
+        Node<T> *temp;
+        while (this->start != NULL) {
+            temp = start;
+            start = start->next;
+            delete temp;
+        }
+    }
 }
 
 //Return the size of this list
 template <class T>
 int List<T>::size(){
+    return this->mySize;
 }
 
 //Return true if this list is empty
 //Otherwise, return false
 template <class T>
 bool List<T>::empty(){
+    if (this->mySize <= 0){
+        return true;
+    } else {
+        return false;
+    }
 }
 
 //Create a new node with value, and insert that new node
 //into this list at start
 template <class T>
 void List<T>::insertStart(T value){
+    if (this->mySize == 0){
+        this->start = new Node<T>(value);
+        ++this->mySize;
+    } else {
+        Node<T> *temp = new Node<T>(value);
+        temp->next = start;
+        start = temp;
+        ++this->mySize;
+    }
 }
 
 //Create a new node with value, and insert that new node
 //into this list at end
 template <class T>
 void List<T>::insertEnd(T value){
+    if (this->mySize == 0){
+        this->start = new Node<T>(value);
+        ++this->mySize;
+    } else {
+        Node<T> *temp = start;
+        while (temp->next != NULL){
+            temp = temp->next;
+        }
+        temp->next = new Node<T>(value);
+        ++this->mySize;
+    }
 }
 
 //Create a new node with value <value>, and insert that new node at position j
 template <class T>
 void List<T>::insertAt(T value, int j){
+    if (j > this->mySize+2){
+        //do nothing - maybe print out an error.
+    } else if (j == 1){
+        this->insertStart(value);
+    } else if (j == this->mySize + 1){
+        this->insertEnd(value);
+        //what should be done if the number is larger than the size by 2 or more?
+    } else {
+        int count = 1;
+        Node<T> *temp = start;
+        while (count < j-1){
+            temp = temp->next;
+            ++count;
+        }
+        
+        Node<T> *newNode = new Node<T>(value);
+        newNode->next = temp->next;
+        temp->next = newNode;
+        ++this->mySize;
+    }
 }
 
 //Remove node at start
 //Make no other changes to list
 template <class T>
 void List<T>::removeStart(){
+    
+    if (this->mySize == 1){
+        delete this->start;
+        start = NULL;
+        --this->mySize;
+    } else if (this->mySize > 1){
+        Node<T> *deleteNode = start;
+        start = start->next;
+        delete deleteNode;
+        --this->mySize;
+    }
 }
 
 //Remove node at end
 //Make no other changes to list
 template <class T>
 void List<T>::removeEnd(){
+    
+    if (this->mySize == 1){
+        delete this->start;
+        start = NULL;
+        --this->mySize;
+    } else if (this->mySize > 1){
+        Node<T> *temp = start;
+        while (temp->next->next != NULL) {
+            temp = temp->next;
+        }
+        delete temp->next;
+        temp->next = NULL;
+        --this->mySize;
+    }
 }
 
 //Remove node at position j
 //Make no other changes to list
 template <class T>
 void List<T>::removeAt(int j){
+    if (j == 1){
+        this->removeStart();
+    } else if (j == this->mySize){
+        this->removeEnd();
+    } else if (j > 0 && j < this->mySize) {
+        int count = 1;
+        Node<T> *temp = start;
+        while (count < j-1){
+            temp = temp->next;
+            ++count;
+        }
+        Node<T> *deleteNode = temp->next;
+        temp->next = temp->next->next;;
+        delete deleteNode;
+        --this->mySize;
+    }
+    
 }
 
 //Return the value of the first node in the Linked List,
 //If no first node, return the default constructed value: T()
 template <class T>
 T List<T>::getFirst(){
+    if (this->mySize > 0){
+        return this->start->value;
+    } else {
+        return T();
+    }
 }
 
 //Return the value of the last node in the Linked List,
 //If no first node, return the default constructed value: T()
 template <class T>
 T List<T>::getLast(){
+    if (this->mySize > 0){
+        Node<T> *temp = start;
+        while (temp->next != NULL){
+            temp = temp->next;
+        }
+        return temp->value;
+    } else {
+        return T();
+    }
 }
 
 //Return the value of the node at position j in the Linked List,
 //If no first node, return the default constructed value: T()
 template <class T>
 T List<T>::getAt(int j){
+    if (this->mySize == 0){
+        return T();
+    } else if (j > this->mySize){
+        return T();
+    } else if (j == 1){
+        return this->getFirst();
+    } else if (j == this->mySize){
+        return this->getLast();
+    } else {
+        int count = 1;
+        Node<T> *temp = start;
+        while (count < j){
+            temp = temp->next;
+            ++count;
+        }
+        return temp->value;
+    }
 }
 
 //Return the position of the (first) node whose value is equal to the key
 //Otherwise, return -1
 template <class T>
 int List<T>::find(T key){
+    if (this->mySize == 0){
+        return -1;
+    } else {
+        int count = 1;
+        Node<T> *temp = this->start;
+        while (temp->value != key && temp->next != NULL ){
+            temp = temp->next;
+            ++count;
+        }
+        
+        if (temp->value == key){
+            return count;
+        } else {
+            return -1;
+        }
+    }
 }
---
+++ cppLinkedList/tester.cpp
@@ -0,0 +1,47 @@
+//
+//  tester.cpp
+//  cppLinkedList
+//
+//  Created by Zach Howes on 9/3/19.
+//  Copyright © 2019 Zach Howes. All rights reserved.
+//
+
+#include <iostream>
+#include "List.h"
+
+
+int main(){
+    List<int> tester;
+//    int num = tester.size();
+//    std::cout<< num <<std::endl;
+//    tester.insertStart(5);
+//    num = tester.size();
+//    std::cout<< num <<std::endl;
+//    std::cout << tester.getFirst() << std::endl;
+    tester.insertStart(2);
+    tester.insertStart(1);
+    tester.insertEnd(3);
+    std::cout<<tester.getFirst()<< " " << tester.getAt(2) << " "<< tester.getLast() << std::endl;
+    tester.insertAt(4, 4);
+    std::cout<<tester.getAt(1)<< " " << tester.getAt(2) << " "<< tester.getAt(3) << " " << tester.getAt(4) << std::endl;
+    
+    std::cout << tester.find(4) << " " << tester.find(2) << " " << tester.find(1) << " " << tester.find(0) << " " << tester.find(3) << std::endl;
+    
+    
+    
+//    tester.removeAt(2);
+//    std::cout<<tester.getAt(1)<< " " << tester.getAt(2) << " "<< tester.getAt(3) << " " << tester.getAt(4) << std::endl;
+//
+//    tester.removeAt(2);
+//       std::cout<<tester.getAt(1)<< " " << tester.getAt(2) << " "<< tester.getAt(3) << " " << tester.getAt(4) << std::endl;
+//
+//    tester.removeAt(5);
+//       std::cout<<tester.getAt(1)<< " " << tester.getAt(2) << " "<< tester.getAt(3) << " " << tester.getAt(4) << std::endl;
+//
+//    tester.removeAt(2);
+//       std::cout<<tester.getAt(1)<< " " << tester.getAt(2) << " "<< tester.getAt(3) << " " << tester.getAt(4) << std::endl;
+//
+//    tester.removeAt(1);
+//       std::cout<<tester.getAt(1)<< " " << tester.getAt(2) << " "<< tester.getAt(3) << " " << tester.getAt(4) << std::endl;
+    return 0;
+}
